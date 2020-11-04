4fa00585ef2408e3b025423240a97097ffd7f890
Mateusz Rydzewski <mateusz.rydzewski@student.vinci.be>
2020-11-04 20:33:17 +0100

    Refractor - toUpper case first letter of classes

---
+++ BerlinClockTest.php
@@ -0,0 +1,8 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class BerlinClockTest extends PHPUnit_Framework_TestCase
+{
+
+}
--- berlinClockTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use PHPUnit\Framework\TestCase;
-
-class berlinClockTest extends PHPUnit_Framework_TestCase
-{
-
-}
