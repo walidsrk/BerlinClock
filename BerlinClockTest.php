cf2846c9cad65c444e7231e94398bee8b1db24b8
Mateusz Rydzewski <mateusz.rydzewski@student.vinci.be>
2020-11-04 19:49:48 +0100

    Refractor - Rename from berlinClock to BerlinClock and from berlinClockTest to BerlinClockTest

---
+++ BerlinClockTest.php
@@ -0,0 +1,12 @@
+<?php
+
+require "vendor/autoload.php";
+require "BerlinClock.php";
+
+use berlinClock\BerlinClock;
+use PHPUnit\Framework\TestCase;
+
+class BerlinClockTest extends TestCase
+{
+
+}
--- berlinClock.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace berlinClock;
-
-class berlinClock
-{
-
-}
