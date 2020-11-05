2c50c2f705e844d0c3f07a01024013912af361fa
walidsrk <serokhwalid@gmail.com>
2020-11-05 12:04:29 +0100

    add - test_fiveMinutes_given20_shouldReturnYYRYOOOOOOO

--- BerlinClock.php
+++ BerlinClock.php
@@ -18,5 +18,6 @@ class BerlinClock
         if($int === 5 || $int === 6) return "YOOOOOOOOOO";
         if($int === 10) return "YYOOOOOOOOO";
         if($int === 15) return "YYROOOOOOOO";
+        if($int === 20) return "YYRYOOOOOOO";
     }
 }
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -52,31 +52,48 @@ class BerlinClockTest extends TestCase
 
     public function test_fiveMinutes_given5_shouldReturnYOOOOOOOOOO(){
         $berlinClock = new BerlinClock();
+
         $actual =$berlinClock->fiveMinutes(5);
+
         $this->assertEquals("YOOOOOOOOOO",$actual);
     }
 
     public function test_simpleMinutes_given6_shouldReturnYOOO(){
         $actual = $this->simpleMinutes(6);
+
         $this->assertEquals("YOOO",$actual);
     }
 
     public function test_fiveMinutes_given6_shouldReturnYOOOOOOOOOO(){
         $berlinClock = new BerlinClock();
+
         $actual = $berlinClock->fiveMinutes(6);
+
         $this->assertEquals("YOOOOOOOOOO",$actual);
     }
 
     public function test_fiveMinutes_given10_shouldReturnYYOOOOOOOOO(){
         $berlinClock = new BerlinClock();
+
         $actual = $berlinClock->fiveMinutes(10);
+
         $this->assertEquals("YYOOOOOOOOO",$actual);
     }
 
-    public function test_fiveMinutes_given15_shouldReturnYYYOOOOOOOO(){
+    public function test_fiveMinutes_given15_shouldReturnYYROOOOOOOO(){
         $berlinClock = new BerlinClock();
+
         $actual = $berlinClock ->fiveMinutes(15);
+
         $this->assertEquals("YYROOOOOOOO", $actual);
+    }
+
+    public function test_fiveMinutes_given20_shouldReturnYYRYOOOOOOO(){
+        $berlinClock = new BerlinClock();
+
+        $actual = $berlinClock ->fiveMinutes(20);
+
+        $this->assertEquals("YYRYOOOOOOO", $actual);
     }
 
 }
