771bebf258f1a8fecd8d370dcd0aa35ff49f335f
Mateusz Rydzewski <mateusz.rydzewski@student.vinci.be>
2020-11-05 15:40:53 +0100

    Delete comparing number 6 in simpleMinutes and fiveMinutes

--- BerlinClock.php
+++ BerlinClock.php
@@ -7,7 +7,7 @@ class BerlinClock
 {
     private function simpleMinutes(int $int):string
     {
-        if($int === 1 || $int === 6) return "YOOO";
+        if($int === 1) return "YOOO";
         if($int === 2) return "YYOO";
         if($int === 3) return "YYYO";
         if($int === 4) return "YYYY";
@@ -15,7 +15,7 @@ class BerlinClock
     }
     private function fiveMinutes(int $int):string
     {
-        if($int === 5 || $int === 6) return "YOOOOOOOOOO";
+        if($int === 5 ) return "YOOOOOOOOOO";
         if($int === 10) return "YYOOOOOOOOO";
         if($int === 15) return "YYROOOOOOOO";
         if($int === 20) return "YYRYOOOOOOO";
